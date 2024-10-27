9209b1ee49126b5b4db517a20508adfe1819020e
sven <42868792+FreerGit@users.noreply.github.com>
2024-10-27 18:53:25 +0100

    add google login flow

--- frontend/app/auth/signin/page.js
+++ frontend/app/auth/signin/page.js
@@ -3,13 +3,11 @@
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { redirect } from "next/navigation";
 
 export default function LoginPage() {
   const { data: session, status } = useSession();
   const router = useRouter();
 
-  // Redirect to "/abc" if session is active and not loading
   useEffect(() => {
     console.log("signin: ", status);
     if (status === "authenticated") {
@@ -21,6 +19,7 @@ export default function LoginPage() {
     <div>
       <h1>Login Page</h1>
       <button onClick={() => signIn("github")}>Login with GitHub</button>
+      <button onClick={() => signIn("google")}>Login with Google</button>
     </div>
   );
 }

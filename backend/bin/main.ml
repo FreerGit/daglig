0db80d6aa05fed305674098b868bafabe6091d44
sven <42868792+FreerGit@users.noreply.github.com>
2024-10-30 23:17:35 +0100

    setup server

--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -1,13 +1,4 @@
 open! Core
 open! Lib
-(*
-   let () =
-  Eio_main.run
-  @@ fun stdenv ->
-  Eio.Switch.run
-  @@ fun sw ->
-  Database.Migration.run_migrations
-    ~sw
-    ~stdenv:(stdenv :> Caqti_eio.stdenv)
-    ~uri:Uri.empty
-;; *)
+
+let () = Lib.Server.run_server ()

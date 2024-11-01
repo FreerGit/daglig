f4e0527e67ebab1d20bcbc73fdda8b51026609c7
sven <42868792+FreerGit@users.noreply.github.com>
2024-11-01 23:47:35 +0100

    setup dev and test container

--- backend/start_db.sh
+++ backend/start_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build -t dev-pg . 
-docker run -p 5432:5432 -d dev-pg
+docker build -t dev-pg .
+docker build -t test-pg .
+docker run -e POSTGRES_DB=test -e POSTGRES_USER=test -e POSTGRES_PASSWORD=test -p 5555:5432 -d test-pg
+docker run -e POSTGRES_DB=dev -e POSTGRES_USER=dev -e POSTGRES_PASSWORD=dev -p 5432:5432 -d dev-pg

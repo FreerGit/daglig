a263b4156a77d5391de72ab03ba68ff266ec8ea6
sven <42868792+FreerGit@users.noreply.github.com>
2024-11-02 20:42:59 +0100

    points graph mock

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,6 +1,8 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
+import { LineChart } from "@mantine/charts";
+
 export default async function LoginPage() {
   const session = await getServerSession();
   console.log(session);
@@ -11,8 +13,52 @@ export default async function LoginPage() {
   }
 
   return (
-    <div>
-      <h1>Your snowball</h1>
+    <div className="flex flex-col items-center mt-8 h-screen">
+      <h1 className="font-semibold text-[54px] mb-4">Your Snowball</h1>
+      <div className="flex w-3/4 h-1/2">
+        <LineChart
+          className="w-full h-full"
+          data={data}
+          dataKey="date"
+          yAxisProps={{ domain: [0, 50] }}
+          series={[{ name: "Points", color: "red" }]}
+        />
+      </div>
     </div>
   );
 }
+
+const data = [
+  {
+    date: "Mar 22",
+    Points: 0,
+  },
+  {
+    date: "Mar 23",
+    Points: 3,
+  },
+  {
+    date: "Mar 24",
+    Points: 5,
+  },
+  {
+    date: "Mar 25",
+    Points: 8,
+  },
+  {
+    date: "Mar 26",
+    Points: 11,
+  },
+  {
+    date: "Mar 27",
+    Points: 9,
+  },
+  {
+    date: "Mar 28",
+    Points: 12,
+  },
+  {
+    date: "Mar 29",
+    Points: 15,
+  },
+];
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,16 +1,35 @@
-import styles from "./styles/landingPage.module.css";
 import Image from "next/image";
 import { Button } from "@mantine/core";
 import { redirect } from "next/navigation";
 import RedirectButton from "./components/RedirectButton";
+import "./styles/globals.css";
 
+// .intro {
+//   text-align: center;
+// }
+
+// .intro h1 {
+//   font-size: 54px;
+//   line-height: 62px;
+//   margin-bottom: 16px;
+// }
+
+// .intro p {
+//   font-size: 22px;
+//   margin-bottom: 48px;
+// }
 export default function LandingPage() {
   return (
     <div>
       <main>
-        <div className={styles.intro}>
-          <h1>Accelarete.</h1>
-          <p> Watch your growth by completing your dailies</p>
+        <div className="text-center mt-8">
+          <h1 className="font-semibold text-[54px] leading-[62px] mb-4">
+            Accelarete.
+          </h1>
+          <p className="text-[22px] mb-12">
+            {" "}
+            Watch your growth by completing your dailies
+          </p>
           {/* <Image priority src={IntroGraph} alt="" /> */}
           <RedirectButton />
         </div>
